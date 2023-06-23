4cee4cb5d4ad4412ee915e5a595a88492c4d9eda
Tom Wechsler <tom.wechsler@outlook.com>
2023-06-23 15:00:14 +0200

    new files

--- PowerShell/Search_Replicating_Directory_Changes_permission.ps1
+++ PowerShell/Search_Replicating_Directory_Changes_permission.ps1
@@ -1,6 +1,6 @@
 #Get all permissions in the domain, filtered to the two critical replication permissions represented by their GUIDs
 Import-Module ActiveDirectory
-cd 'AD:DC=prime,DC=pri' # Replace with distinguished name of your domain
+Set-Location 'AD:DC=prime,DC=pri' # Replace with distinguished name of your domain
 $AllReplACLs = (Get-AcL).Access | Where-Object {$_.ObjectType -eq '1131f6ad-9c07-11d1-f79f-00c04fc2dcd2' -or $_.ObjectType -eq '1131f6aa-9c07-11d1-f79f-00c04fc2dcd2'}
 
 #Filter this list to RIDs above 1000 which will exclude well-known Administrator groups
